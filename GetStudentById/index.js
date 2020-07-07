0d5ab2c077737157a3ae364e67f0d2f3580556bf
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-07 15:58:17 -0300

    refactor: implements find student by id

--- GetStudentById/index.js
+++ GetStudentById/index.js
@@ -1,13 +1,16 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const { ObjectID } = require('mongodb');
+const createMongoClient = require('../shared/mongoClient');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+module.exports = async function (context, req) {
+    const { id } = req.params;
+    
+    const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+    const Students = MongoClient.collection('student');
+    const res = await Students.findOne({ _id: ObjectID(id) });
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+        status: 200,
+        body: res
+    }
+};
