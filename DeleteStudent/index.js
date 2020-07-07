cb42d63a1c9b54de0cbef5c81e050ed5752d25ba
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-07 17:04:38 -0300

    refactor: implements delete student

--- DeleteStudent/index.js
+++ DeleteStudent/index.js
@@ -1,13 +1,17 @@
+const { ObjectID } = require('mongodb');
+const createMongoClient = require('../shared/mongoClient');
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  const { id } = req.params;
+  const student = req.body;
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+  const Students = MongoClient.collection('student');
+  const res = await Students.findOneAndDelete({ _id: ObjectID(id) });
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  closeConnectionFn();
+  context.res = {
+    status: 200,
+    body: res
+  };
 }
