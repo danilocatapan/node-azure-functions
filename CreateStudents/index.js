9bd6e4af02cdfd842085539883476f640b5c20e5
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-07 16:12:25 -0300

    refactor: implements create student

--- CreateStudents/index.js
+++ CreateStudents/index.js
@@ -1,13 +1,16 @@
+const createMongoClient = require('../shared/mongoClient');
+
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  const student = req.body;
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+  const Students = MongoClient.collection('student');
+  const res = await Students.insert(student);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+  closeConnectionFn();
+  context.res = {
+    status: 201,
+    body: res,
+  };
+};
