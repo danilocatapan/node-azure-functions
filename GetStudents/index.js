d3aa3155c2eddcea68818aeb228e49720dbd4d24
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-07-07 15:45:06 -0300

    refactor: products to students

--- GetStudents/index.js
+++ GetStudents/index.js
@@ -2,8 +2,8 @@ const createMongoClient = require('../shared/mongoClient');
 
 module.exports = async function (context, req) {
   const { client: MongoClient, closeConnectionFn } = await createMongoClient();
-  const Products = MongoClient.collection('student');
-  const res = await Products.find({});
+  const Students = MongoClient.collection('student');
+  const res = await Students.find({});
   const body = await res.toArray();
 
   context.res = {
